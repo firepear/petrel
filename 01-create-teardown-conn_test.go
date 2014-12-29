51fe43560340c23eb30f7334791d22bc0811295f
Shawn Boyette <shawn@firepear.net>
2014-12-28 21:46:53 -0500

    error msg changed

--- 01-create-teardown-conn_test.go
+++ 01-create-teardown-conn_test.go
@@ -31,7 +31,7 @@ func TestConnHandler(t *testing.T) {
 	if msg.Err == nil {
 		t.Errorf("connection drop should be an err, but got nil")
 	}
-	if msg.Txt != "adminsock connection lost" {
+	if msg.Txt != "adminsock connection dropped" {
 		t.Errorf("unexpected msg.Txt: %v", msg.Txt)
 	}
 	// shut down adminsocket
