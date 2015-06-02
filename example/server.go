f7df53db9f25b45e64f20a663f6dedc50b9e767b
Shawn Boyette <shawn@firepear.net>
2015-06-02 16:25:18 -0400

    server working a little better now

--- example/server.go
+++ example/server.go
@@ -44,25 +44,64 @@ func main() {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	// create the network-facing asock instance
+	// create the network-facing asock instance. First set up the
+	// Dispatch struct.
 	d := make(asock.Dispatch)
 	d["split"] = &asock.DispatchFunc{echosplit, "split"}
 	d["nosplit"] = &asock.DispatchFunc{echonosplit, "nosplit"}
 	d["time"] = &asock.DispatchFunc{telltime, "nosplit"}
-
+	// then the Asock configuration
 	c := asock.Config{
 		Sockname: *socket,
 		Msglvl: asock.All,
 	}
+	// then do the instantiation
 	as, err := asock.NewTCP(c, d)
 	if err != nil {
 		panic(err)
 	}
-
-	// asock instance is live. serve until an interrupt/kill signal
-	// arrives
 	log.Println("Asock instance is serving.")
-	<-sigchan
-	log.Println("OS signal received; shutting down")
-	as.Quit()
+	// no error, so the asock instance is live. spin up our Msgr handler.
+	msgchan := make(chan error, 1)
+	go msgHandler(as, sigchan, msgchan)
+	msg := <-msgchan
+	log.Println("Asock instance has shut down. Last Msg received was:")
+	log.Println(msg)
+}
+
+func msgHandler(as *asock.Asock, sigchan chan os.Signal, msgchan chan error) {
+	var msg *asock.Msg
+	keepgoing := true
+Loop:
+	for keepgoing {
+		select {
+		case msg = <-as.Msgr:
+			switch msg.Code {
+			case 599:
+				// 599 is "the asock listener has died". this means
+				// we're not accepting connections anymore. shutdown
+				// our Asock and break out of the 'for' so our program
+				// knows what happened.
+				as.Quit()
+				keepgoing = false
+				msgchan <- msg
+				break Loop
+			case 199:
+				// 199 is "we've been told to quit", so break out of
+				// the 'for'.
+				keepgoing = false
+				msgchan <- msg
+				break Loop
+			default:
+				log.Println(msg)
+			}
+		case <- sigchan:
+			// we've trapped a signal from the OS. tell our Asock to
+			// shut down. a subsequent pass through this loop should
+			// be a Msg with Code = 199, which will terminate the loop
+			// adn return from this function.
+			log.Println("OS signal received; shutting down")
+			as.Quit()
+		}
+	}
 }
