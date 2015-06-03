e49de78d64a4194c908e89d72e01c9268e09ca7b
Shawn Boyette <shawn@firepear.net>
2015-06-03 00:28:31 -0400

    documentation

--- example/client.go
+++ example/client.go
@@ -1,5 +1,9 @@
 package main
 
+// This is a simple, one-shot client. It connects, sends its
+// non-option arguments as a request, prints the result it gets, and
+// exits.
+
 import (
 	"flag"
 	"fmt"
@@ -12,15 +16,23 @@ func main() {
 	// handle command line args
 	var socket = flag.String("socket", "localhost:60606", "Addr:port to bind the socket to")
 	flag.Parse()
+
+	// set up configuration and create client instance
 	conf := client.Config{Addr: *socket}
 	c, err := client.NewTCP(conf)
 	if err != nil {
 		panic(err)
 	}
+
+	// stitch together the non-option arguments into our request
 	req := strings.Join(flag.Args(), " ")
+
+	// and dispatch it to the server!
 	resp, err := c.Dispatch([]byte(req))
 	if err != nil {
 		panic(err)
 	}
+
+	// print out what we got back and exit
 	fmt.Println(string(resp))
 }
