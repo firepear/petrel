e4ab30319e65fdcef177ad51f2e0e044de869ff5
Shawn Boyette <shawn@firepear.net>
2016-10-14 01:32:38 -0600

    updated demo client/server

--- demo/client.go
+++ demo/client.go
@@ -1,12 +1,13 @@
 package main
 
-// This is a simple, one-shot client. It connects, sends its
+// this is a simple, one-shot client. it connects, sends its
 // non-option arguments as a request, prints the result it gets, and
 // exits.
 
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 
 	"firepear.net/petrel"
@@ -14,24 +15,26 @@ import (
 
 func main() {
 	// handle command line args
-	var socket = flag.String("socket", "localhost:60606", "Addr:port to bind the socket to")
+	var socket = flag.String("socket", "localhost:60606", "Where to bind the socket (addr:port)")
 	flag.Parse()
 
 	// set up configuration and create client instance
 	conf := &petrel.ClientConfig{Addr: *socket}
 	c, err := petrel.TCPClient(conf)
 	if err != nil {
-		panic(err)
+		fmt.Printf("can't initialize client: %s\n", err)
+		os.Exit(1)
 	}
 	defer c.Quit()
 
-	// stitch together the non-option arguments into our request
-	req := strings.Join(flag.Args(), " ")
+	// stitch together the non-option arguments into a request
+	req := []byte(strings.Join(flag.Args(), " "))
 
 	// and dispatch it to the server!
-	resp, err := c.Dispatch([]byte(req))
+	resp, err := c.Dispatch(req)
 	if err != nil {
-		panic(err)
+		fmt.Printf("did not get successful response: %s\n", err)
+		os.Exit(1)
 	}
 
 	// print out what we got back and exit
--- demo/server.go
+++ demo/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"log"
 	"os"
@@ -12,147 +11,139 @@ import (
 	"firepear.net/petrel"
 )
 
+
+// echonosplit is one of the functions we'll use as Responders after
+// we instantiate a Server. it's an echo function, with an argmode of
+// "blob".
+func echonosplit(args [][]byte) ([]byte, error) {
+	return args[0], nil
+}
+
+// telltime, our other Responder, returns the current datetime
+func telltime(args [][]byte) ([]byte, error) {
+	return []byte(time.Now().Format(time.RFC3339)), nil
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+// msgHandler is a function which we'll launch later on as a
+// goroutine. It listens to our Server's Msgr channel, checking for a
+// few critical things and logging everything else informationally.
+func msgHandler(s *petrel.Server, msgchan chan error) {
+	var msg *petrel.Msg
+	keepalive := true
+
+	for keepalive {
+		// wait on a Msg to arrive and do a switch based on
+		// its status code.
+		msg = <-s.Msgr
+		switch msg.Code {
+		case 599:
+			// 599 is "the Server listener socket has
+			// died". this means we're not accepting
+			// connections anymore. call s.Quit() to clean
+			// things up, send the Msg to our main
+			// routine, then kill this loop
+			s.Quit()
+			keepalive = false
+			msgchan <- msg
+		case 199:
+			// 199 is "we've been told to quit", so we
+			// want to break out of the loop here as well
+			keepalive = false
+			msgchan <- msg
+		default:
+			// anything else we'll log to the console to
+			// show what's going on under the hood!
+			log.Println(msg)
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 func main() {
 	// first, handle command line args
 	var socket = flag.String("socket", "localhost:60606", "Addr:port to bind the socket to")
 	flag.Parse()
 
-	// now let's give ourselves a way to shut down. we'll listen for
-	// SIGINT and SIGTERM, so we can behave like a proper service
-	// (mostly -- we're not writing out a pidfile). anyway, to do that
-	// we need a channel to recieve signal notifications on.
+	// now this part has nothing to do with Petrel, but we'll
+	// listen for SIGINT and SIGTERM so we can behave like a
+	// proper service. (mostly; we're not writing out a pidfile.)
+	// anyway, we need a channel to recieve signals on.
 	sigchan := make(chan os.Signal, 1)
-	// and then we register sigchan to listen for the signals we want.
+	// and we need to register that channel to listen for the
+	// signals we want.
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-	// we now respond properly to 'kill' calls to our pid, and to C-c
-	// at the terminal we're running in.
+	// we will now respond properly to 'kill(1)' calls to our pid,
+	// and to C-c at the terminal we're running in.
 
-	// with that done, we can set up our Petrel instance.  first we set
-	// up the configuration
-	c := &petrel.Config{
+	// with that done, we can set up our Petrel instance.  first
+	// create a configuration
+	c := &petrel.ServerConfig{
 		Sockname: *socket,
 		Msglvl: petrel.All,
 	}
-	// and then we call the constructor! in a real server, obviously,
-	// you wouldn't want to just panic.
-	h, err := petrel.NewTCP(c)
+
+	// then instantiate a Server.
+	s, err := petrel.TCPServer(c)
 	if err != nil {
-		panic(err)
+		log.Printf("could not instantiate Server: %s\n", err)
+		os.Exit(1)
 	}
-	// and add our command handlers, using a map for convenient error
-	// checking
-	handlers := map[string]func([][]byte) ([]byte, error){
-		"split": echosplit,
-		"nosplit": echonosplit,
-		"time": telltime,
-		"badcmd": thisfuncerrs,
+
+	// then Register our Responders with the Server
+	err = s.Register("echo", "blob", echonosplit)
+	if err != nil {
+		log.Printf("failed to register responder 'echo': %s", err)
+		os.Exit(1)
 	}
-	for name, function := range handlers {
-		err = h.AddFunc(name, "args", function)
-		if err != nil {
-			panic(err)
-		}
+	err = s.Register("time", "blob", telltime)
+	if err != nil {
+		log.Printf("failed to register responder 'echo': %s", err)
+		os.Exit(1)
 	}
+	// now, if a client sends a request starting with "echo", the
+	// request will be dispatched to echonosplit. likewise "time"
+	// and telltime.
 	log.Println("Petrel handler is serving.")
 
-	// at this point, our Handler is listening and ready to do its
-	// thing. it's time to spin up an event loop which listens to
-	// h.Msgr, which is how the Handler tells us what it's
-	// doing. first we need a channel so that we can get *some*
-	// messages out of that loop. why is it a 'chan error' instead of
-	// a 'chan petrel.Msg'?  petrel.Msg implements error, so we *can*,
-	// basically. less typing.
+	// the Sandler is now listening and ready to do work.  it's
+	// time to start msgHandler, the event loop we defined
+	// earlier. we hand it a channel that it uses to pass
+	// important Msgs to the main event loop, which is coming up
+	// next.  it's a 'chan error' instead of a 'chan petrel.Msg'
+	// because petrel.Msg implements the error interface.
 	msgchan := make(chan error, 1)
-	// now launch the handler as a goroutine.
-	go msgHandler(h, msgchan)
+	go msgHandler(s, msgchan)
 
-	// this is our *main* eventloop. since we're a bare-bones example
-	// server, we just do a select on msgchan and sigchan, waiting on
-	// a notification via one of those channels.  as part of a "real"
-	// application, there would be more things to handle here.
+	// and here is the main eventloop. it's simply a select on
+	// msgchan and sigchan, so that we can handle shutdown
+	// cleanly. the work of handling requests is entirely inside
+	// Petrel, and requires no application logic or intervention.
 	keepalive := true
 	for keepalive {
 		select {
 		case msg := <-msgchan:
-			// we've been handed a Msg over msgchan, which means that
-			// our Handler has shut itself down for some reason. if this
-			// were a more robust server, we would modularize Handler
-			// creation and this eventloop, so that should we trap a
-			// 599 we could spawn a new Handler and launch it in this
-			// one's place. but we're just gonna exit this loop,
-			// causing main() to terminate, and with it the server
-			// instance.
-			log.Println("Handler has shut down. Last Msg received was:")
-			log.Println(msg)
+			// we've been handed a Msg over msgchan, which
+			// means that our Server has shut itself down
+			// for some reason. we're going to exit this
+			// loop, causing main() to terminate.
+			log.Printf("Handler has shut down. Last Msg received was: %s", msg)
 			keepalive = false
 			break
 		case <- sigchan:
-			// we've trapped a signal from the OS. tell our Handler to
-			// shut down, but don't exit the eventloop because we want
-			// to handle the Msgs which will be incoming.
+			// we've trapped a signal from the OS. tell
+			// our Server to shut down, but don't exit the
+			// eventloop because we want to handle the
+			// Msgs which will be incoming -- including
+			// the one we'll get on msgchan once the
+			// Server has finished its work.
 			log.Println("OS signal received; shutting down")
-			h.Quit()
+			s.Quit()
 		}
-		// there's no default case in the select, as that would cause
-		// it to be nonblocking. and that would cause main() to exit
-		// immediately.
+		// there's no default case in the select, as that
+		// would make it nonblocking, which would in turn
+		// cause main() to exit immediately.
 	}
-}
-
-func msgHandler(h *petrel.Handler, msgchan chan error) {
-	// our Msg handler function is very simple. it's almost a clone of
-	// the main eventloop. first we just create a couple of variables
-	// to hold Msgs and to control the for loop.
-	var msg *petrel.Msg
-	keepalive := true
-
-	for keepalive {
-		// then we wait on a Msg to arrive and do a switch based on
-		// its status code.
-		msg = <-h.Msgr
-		switch msg.Code {
-		case 599:
-			// 599 is "the Handler listener has died". this means we're
-			// not accepting connections anymore. call h.Quit() to
-			// clean things up, send the Msg to our main routine, then
-			// kill this for loop
-			h.Quit()
-			keepalive = false
-			msgchan <- msg
-		case 199:
-			// 199 is "we've been told to quit", so we want to break
-			// out of the 'for' here as well
-			keepalive = false
-			msgchan <- msg
-		default:
-			// anything else we just log!
-			log.Println(msg)
-		}
-	}
-}
-
-// this handler is an echo function, with an argmode of "split".
-func echosplit(args [][]byte) ([]byte, error) {
-	var b []byte
-	for _, arg := range args {
-		b = append(b, arg...)
-		b = append(b, 32)
-	}
-	return b, nil
-}
-
-// this handler is an echo function, with an argmode of
-// "nosplit".
-func echonosplit(args [][]byte) ([]byte, error) {
-	return args[0], nil
-}
-
-// this one just returns the current datetime
-func telltime(args [][]byte) ([]byte, error) {
-	return []byte(time.Now().Format(time.RFC3339)), nil
-}
-
-// and this one returns an error
-func thisfuncerrs(args [][]byte) ([]byte, error) {
-	return nil, errors.New("Something went wrong inside me!")
 }
