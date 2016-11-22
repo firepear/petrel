c72a7f9aaa59e510ec4506597b12fe5bd779bd78
Shawn Boyette <shawn@firepear.net>
2016-11-22 13:14:43 -0700

    first draft of shim

--- demo/03-petreljs/wsshim.go
+++ demo/03-petreljs/wsshim.go
@@ -25,7 +25,7 @@ func petrelShim(ws *websocket.Conn) {
 
 	pc, err := petrel.TCPClient(&petrel.ClientConfig{Addr: *paddr, Timeout: 50})
 	if err != nil {
-		log.Printf("%d: couldn't instantiate petrel client; bailing: %s", connnum, err)
+		log.Printf("%d: couldn't instantiate petrel client; bailing: %s\n", connnum, err)
 		os.Exit(1)
 	}
 
@@ -36,8 +36,9 @@ WSLoop:
 		for {
 			n, err := ws.Read(b1)
 			if err != nil {
-				log.Printf("%d: couldn't read from websocket; closing conn: %s", connnum, err)
-				ws.Close();
+				log.Printf("%d: closing conn: couldn't read from websocket: %s\n", connnum, err)
+				ws.Close()
+				pc.Quit()
 				break WSLoop
 			}
 			if n < 128 {
@@ -46,6 +47,23 @@ WSLoop:
 			b2 = append(b2, b1...)
 		}
 		b2 = append(b2, b1[:n]...)
+		resp, err := pc.Dispatch(b2)
+		log.Printf("%d: dispatched request from ws to petrel", connnum)
+		if err != nil {
+			log.Printf("%d: closing conn: couldn't read from petrel: %s\n", connnum, err)
+			ws.Close()
+			pc.Quit()
+			break WSLoop
+		}
+		log.Printf
+		n, err = ws.Write(resp)
+		if err != nil {
+			log.Printf("%d: closing conn: couldn't write to websocket: %s\n", connnum, err)
+			ws.Close()
+			pc.Quit()
+			break WSLoop
+		}
+		log.Printf("%d: relayed response from petrel to ws", connnum)
 	}
 }
 
