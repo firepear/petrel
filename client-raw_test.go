099580c7b8018ae598604526a7b2283f3221a779
Shawn Boyette <shawn@firepear.net>
2016-11-25 16:29:01 -0700

    adding raw mode client test

---
+++ client-raw_test.go
@@ -0,0 +1,54 @@
+package petrel
+
+import (
+	"testing"
+)
+
+func TestRawClientNewTCP(t *testing.T) {
+	// instantiate TCP petrel
+	asconf := &ServerConfig{Sockname: "127.0.0.1:10298", Msglvl: Fatal}
+	as, err := TCPServer(asconf)
+	if err != nil {
+		t.Errorf("Failed to create petrel instance: %v", err)
+	}
+	as.Register("echo", "blob", hollaback)
+	// and now a client
+	cconf := &ClientConfig{Addr: "127.0.0.1:10298"}
+	c, err := TCPClient(cconf)
+	if err != nil {
+		t.Errorf("Failed to create client: %v", err)
+	}
+	// and send a message
+	c.Seq++
+	xmission, interr, err := marshalXmission([]byte("echo just the one test"), nil, c.Seq)
+	if err != nil {
+		t.Errorf("marshalXmission returned error: %s - %s", interr, err)
+	}
+	resp, err := c.DispatchRaw(xmission)
+	if err != nil {
+		t.Errorf("Dispatch returned error: %v", err)
+	}
+	// check sequence
+	xseq := []byte{1, 0, 0, 0}
+	rseq := resp[0:4]
+	for i := 0; i < 4; i++ {
+		if rseq[i] != xseq[i] {
+			t.Errorf("Byte %d of received seq should be %d but is %d", i, xseq[i], rseq[i])
+		}
+	}
+	xplen := []byte{17, 0, 0, 0}
+	rplen := resp[4:8]
+	for i := 0; i < 4; i++ {
+		if rplen[i] != xplen[i] {
+			t.Errorf("Byte %d of received plen should be %d but is %d", i, xplen[i], rplen[i])
+		}
+	}
+	if resp[8] != Protover {
+		t.Errorf("Received protover should be %d but is %d", Protover, resp[8])
+	}
+	if string(resp[9:]) != "just the one test" {
+		t.Errorf("Expected `just the one test` but got: `%v`", string(resp))
+	}
+	c.Quit()
+	as.Quit()
+}
